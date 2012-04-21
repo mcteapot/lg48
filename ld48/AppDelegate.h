6dd0d140e3716923498f548af05fa5f367ce01c1
Arjun Prakash <archieoi@gmail.com>
2012-04-21 12:44:33 -0700

    Initial Commit

---
+++ ld48/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  ld48
+//
+//  Created by arjun prakash on 4/21/12.
+//  Copyright CyborgDino 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
